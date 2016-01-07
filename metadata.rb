9b07708fb341f771e9e1a7e9952578d74ef28214
nanopico-san <nanopico.san@gmail.com>
2016-01-08 08:27:21 +0900

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name 'git-client'
+maintainer 'ᑎɑղօԹíϲօ(nanopico)'
+maintainer_email 'nanopico.san@gmail.com'
+license 'Apache 2.0'
+description 'Installs/Configures git-client setteings on your environment.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.1'
+version '0.1.0'
